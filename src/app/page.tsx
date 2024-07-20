8e82a4e3202bef8939a184210a434c8226854806
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-07-20 16:15:56 +0300

    remove .env file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import {
-  createProjectAction,
-  getAllProjectsAction,
-} from "@/actions/project.action";
-
 export default async function Home() {
-  const projects = await getAllProjectsAction();
-  console.log(projects);
   return <main>Mostafa</main>;
 }
